9abda53fa7e47be6b05347fb9367f329d00af234
Reed Banks <rexxstark20@gmail.com>
2025-07-21 10:54:01 +0000

    added changes in Voice_turtle/main.py and fixed path errors when using notebook in vscode.

--- demos/Voice_turtle/main.py
+++ demos/Voice_turtle/main.py
@@ -2,10 +2,24 @@ import turtle
 
 t = turtle.Turtle()
 
+def close_on_esc():
+    """
+        close when esc key is pressed
+    """
+    turtle.bye()
+
+t.color("blue")
+t.begin_fill()
+
 for _ in range(4):
+    t.fillcolor("#35b96a")
     t.forward(100)
     t.right(90)
-    t.forward(100)
 
-t.right(90)
-t.forward(100)
+t.end_fill()
+    
+turtle.listen()
+turtle.onkeypress(close_on_esc,"Escape")
+turtle.mainloop()
+
+    
